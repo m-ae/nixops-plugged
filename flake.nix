81d4d3dfc5046f95b8d830a5f193c135bc5fd8c1
Iury Fukuda <zbioe@protonmail.com>
2021-05-06 16:06:41 -0300

    only vbox

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "NixOps with several plugins installed.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+
+  outputs = { self, flake-compat, nixpkgs, poetry2nix, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ poetry2nix.overlay ];
+        };
+        nixopsPluggable = import ./nixops-pluggable.nix pkgs;
+
+        inherit (nixopsPluggable) overrides nixops;
+      in rec {
+
+        defaultApp = {
+          type = "app";
+          program = "${packages.nixops-plugged}/bin/nixops";
+        };
+
+        defaultPackage = packages.nixops-plugged;
+
+        packages = {
+          # A nixops with all plugins included.
+          nixops-plugged = nixops.withPlugins (ps:
+            [
+              # ps.nixops-aws
+              # ps.nixops-digitalocean
+              # ps.nixops-gcp
+              # ps.nixops-hetznercloud
+              # ps.nixops-virtd
+              ps.nixopsvbox
+            ]);
+          # A nixops with each plugin for users who use a single provider.
+          # Benefits from a much faster download/install.
+          # nixops-aws = nixops.withPlugins (ps: [ps.nixops-aws]);
+          # nixops-gcp = nixops.withPlugins (ps: [ps.nixops-gcp]);
+          # nixops-digitalocean = nixops.withPlugins (ps: [ps.nixops-digitalocean]);
+          # nixops-hetznercloud = nixops.withPlugins (ps: [ps.nixops-hetznercloud]);
+          # nixops-virtd = nixops.withPlugins (ps: [ps.nixops-virtd]);
+          nixopsvbox = nixops.withPlugins (ps: [ ps.nixopsvbox ]);
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            (pkgs.poetry2nix.mkPoetryEnv {
+              inherit overrides;
+              projectDir = ./.;
+            })
+            pkgs.poetry
+          ];
+        };
+
+      });
+}
---
+++ nixops-pluggable.nix
@@ -0,0 +1,104 @@
+pkgs:
+
+let
+
+  inherit (pkgs) lib;
+
+  # Make a python derivation pluginable
+  #
+  # This adds a `withPlugins` function that works much like `withPackages`
+  # except it only links binaries from the explicit derivation /share
+  # from any plugins
+  toPluginAble = { drv, finalDrv, final }: drv.overridePythonAttrs(old: {
+    passthru = old.passthru // {
+      # Wrap the buildEnv derivation in an outer derivation that omits interpreters & other binaries
+      withPlugins = pluginFn:
+        let
+          plugins = [finalDrv] ++ pluginFn final;
+          # The complete buildEnv drv
+          buildEnvDrv = interpreter.buildEnv.override { extraLibs = plugins; };
+          # Create a separate environment aggregating the share directory
+          # This is done because we only want /share for the actual plugins
+          # and not for e.g. the python interpreter and other dependencies.
+          manEnv = pkgs.symlinkJoin {
+            name = "${finalDrv.pname}-plugged-share-${finalDrv.version}";
+            preferLocalBuild = true;
+            allowSubstitutes = false;
+            paths = plugins;
+            postBuild = ''
+              if test -e $out/share; then
+                mv $out out
+                mv out/share $out
+              else
+                rm -r $out
+                mkdir $out
+              fi
+              '';
+          };
+        in
+          pkgs.runCommandNoCC
+            "${finalDrv.pname}-plugged-${finalDrv.version}"
+            { inherit (finalDrv) passthru meta; }
+            ''
+              mkdir -p $out/bin
+
+              for bindir in ${lib.concatStringsSep " " (map (d: "${lib.getBin d}/bin") plugins)}; do
+                for bin in $bindir/*; do
+                  ln -s ${buildEnvDrv}/bin/$(basename $bin) $out/bin/
+                done
+              done
+
+              ln -s ${manEnv} $out/share
+              '';
+    };
+  });
+
+  interpreter = (pkgs.poetry2nix.mkPoetryPackages {
+    inherit overrides;
+    projectDir = ./.;
+  }).python;
+
+  overrides = pkgs.poetry2nix.overrides.withDefaults (final: prev: {
+    nixops = toPluginAble {
+      inherit final;
+      finalDrv = final.nixops;
+      # Attach meta to nixops
+      drv = prev.nixops.overridePythonAttrs (old: {
+        format = "pyproject";
+        buildInputs = old.buildInputs ++ [ final.poetry ];
+        meta = old.meta // {
+          homepage = https://github.com/NixOS/nixops;
+          description = "NixOS cloud provisioning and deployment tool";
+          maintainers = with lib.maintainers; [ aminechikhaoui eelco rob domenkozar ];
+          platforms = lib.platforms.unix;
+          license = lib.licenses.lgpl3;
+        };
+      });
+    };
+
+    # These are included in the latest poetry2nix overlay.
+
+    jsonpickle = prev.jsonpickle.overridePythonAttrs (old: {
+      nativeBuildInputs = old.nativeBuildInputs ++ [final.toml];
+    });
+
+    # https://github.com/nix-community/poetry2nix/issues/218
+    packaging = prev.packaging.overridePythonAttrs({buildInputs ? [], ...}: {
+      format = "pyproject";
+      buildInputs = buildInputs ++ [ final.flit-core ];
+    });
+
+    # https://github.com/nix-community/poetry2nix/issues/208
+    #typeguard = prev.typeguard.overridePythonAttrs (old: {
+    #  postPatch = ''
+    #    substituteInPlace setup.py \
+    #      --replace 'setup()' 'setup(version="${old.version}")'
+    #  '';
+    #});
+
+  });
+
+in {
+  inherit overrides;
+  inherit (interpreter.pkgs) nixops;
+}
