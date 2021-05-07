e8376e288c9e9ec861059fe3d27dfcc0d2795d2f
Iury Fukuda <zbioe@protonmail.com>
2021-05-07 00:44:29 -0300

    add virtd

--- flake.nix
+++ flake.nix
@@ -31,22 +31,21 @@
 
         packages = {
           # A nixops with all plugins included.
-          nixops-plugged = nixops.withPlugins (ps:
-            [
-              # ps.nixops-aws
-              # ps.nixops-digitalocean
-              # ps.nixops-gcp
-              # ps.nixops-hetznercloud
-              # ps.nixops-virtd
-              ps.nixopsvbox
-            ]);
+          nixops-plugged = nixops.withPlugins (ps: [
+            # ps.nixops-aws
+            # ps.nixops-digitalocean
+            # ps.nixops-gcp
+            # ps.nixops-hetznercloud
+            ps.nixops-virtd
+            ps.nixopsvbox
+          ]);
           # A nixops with each plugin for users who use a single provider.
           # Benefits from a much faster download/install.
           # nixops-aws = nixops.withPlugins (ps: [ps.nixops-aws]);
           # nixops-gcp = nixops.withPlugins (ps: [ps.nixops-gcp]);
           # nixops-digitalocean = nixops.withPlugins (ps: [ps.nixops-digitalocean]);
           # nixops-hetznercloud = nixops.withPlugins (ps: [ps.nixops-hetznercloud]);
-          # nixops-virtd = nixops.withPlugins (ps: [ps.nixops-virtd]);
+          nixops-virtd = nixops.withPlugins (ps: [ ps.nixops-virtd ]);
           nixopsvbox = nixops.withPlugins (ps: [ ps.nixopsvbox ]);
         };
 
