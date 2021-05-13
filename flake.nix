996e8cac5c6d5a7a88f0cc11faca05f5966a3d49
Iury Fukuda <zbioe@protonmail.com>
2021-05-13 02:53:29 -0300

    remove virtd and pin versions

--- flake.nix
+++ flake.nix
@@ -1,12 +1,18 @@
 {
   description = "NixOps with several plugins installed.";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
-  inputs.utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url =
+    "github:NixOS/nixpkgs/361bea3f007e2d3e31e185f6ae246357e4f19402";
+
+  inputs.poetry2nix.url =
+    "github:nix-community/poetry2nix/2d27d44397242b28c3f0081e0432e4f6c951f3a1";
+
+  inputs.utils.url =
+    "github:numtide/flake-utils/b543720b25df6ffdfcf9227afafc5b8c1fabfae8";
 
   inputs.flake-compat = {
-    url = "github:edolstra/flake-compat";
+    url =
+      "github:edolstra/flake-compat/99f1c2157fba4bfe6211a321fd0ee43199025dbf";
     flake = false;
   };
 
@@ -31,21 +37,22 @@
 
         packages = {
           # A nixops with all plugins included.
-          nixops-plugged = nixops.withPlugins (ps: [
-            # ps.nixops-aws
-            # ps.nixops-digitalocean
-            # ps.nixops-gcp
-            # ps.nixops-hetznercloud
-            ps.nixops-virtd
-            ps.nixopsvbox
-          ]);
+          nixops-plugged = nixops.withPlugins (ps:
+            [
+              # ps.nixops-aws
+              # ps.nixops-digitalocean
+              # ps.nixops-gcp
+              # ps.nixops-hetznercloud
+              # ps.nixops-virtd
+              ps.nixopsvbox
+            ]);
           # A nixops with each plugin for users who use a single provider.
           # Benefits from a much faster download/install.
           # nixops-aws = nixops.withPlugins (ps: [ps.nixops-aws]);
           # nixops-gcp = nixops.withPlugins (ps: [ps.nixops-gcp]);
           # nixops-digitalocean = nixops.withPlugins (ps: [ps.nixops-digitalocean]);
           # nixops-hetznercloud = nixops.withPlugins (ps: [ps.nixops-hetznercloud]);
-          nixops-virtd = nixops.withPlugins (ps: [ ps.nixops-virtd ]);
+          # nixops-virtd = nixops.withPlugins (ps: [ ps.nixops-virtd ]);
           nixopsvbox = nixops.withPlugins (ps: [ ps.nixopsvbox ]);
         };
 
